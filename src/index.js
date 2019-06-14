f6123f2f9a1968b0b66f15d7456382bfa942934f
Michael Kim <m.kim4247@gmail.com>
2019-06-13 20:07:58 -0400

    basic btn added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(){
   console.log("connected")
+
 })
+
+let main = document.getElementById("main")
+let btn1 = document.getElementById("btn1")
+
+bt1n.addEventListener('click', () => console.log('clicked'))
