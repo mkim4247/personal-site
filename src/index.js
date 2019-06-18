17c202d87f5baa85a06ba313fa6b4c5be25297c8
Michael Kim <m.kim4247@gmail.com>
2019-06-18 17:31:42 -0400

    almost done with landing page

--- src/index.js
+++ src/index.js
@@ -1,149 +1,198 @@
 document.addEventListener("DOMContentLoaded", function(){
   console.log("connected")
+  main()
 
 })
+function main(){
 
-let canvas = document.querySelector('canvas')
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-let c = canvas.getContext('2d');
-
-let mouse = {
-  x: null,
-  y: null
-}
-
-let maxRadius = 50;
-
-let colors = [
-  '#87BCDE',
-  '#BFACAA',
-  '#02020A',
-  '#05204A',
-  '#E1E2EF'
-]
-
-window.addEventListener('mousemove', function(event){
-  mouse.x = event.x
-  mouse.y = event.y
-})
-
-window.addEventListener('resize', function(){
+  let canvas = document.querySelector('canvas')
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
-  init()
-})
+  let c = canvas.getContext('2d');
 
-
-class Circle {
-  constructor(x, y, dx, dy, radius){
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
-    this.radius = radius;
-    this.minRadius = radius;
-    this.color = colors[Math.floor(Math.random() * colors.length)]
-  }
-  draw = function() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
+  let mouse = {
+    x: null,
+    y: null
   }
 
-  update = function(){
-    if(this.x + this.radius > innerWidth || this.x - this.radius < 0){
-      this.dx = -this.dx;
+  let maxRadius = 50;
+
+  let colors = [
+    '#87BCDE',
+    '#BFACAA',
+    '#02020A',
+    '#05204A',
+    '#E1E2EF'
+  ]
+
+  window.addEventListener('mousemove', function(event){
+    mouse.x = event.x
+    mouse.y = event.y
+  })
+
+  window.addEventListener('resize', function(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    init()
+  })
+
+  class Circle {
+    constructor(x, y, dx, dy, radius){
+      this.x = x;
+      this.y = y;
+      this.dx = dx;
+      this.dy = dy;
+      this.radius = radius;
+      this.minRadius = radius;
+      this.color = colors[Math.floor(Math.random() * colors.length)]
+    }
+    draw = function() {
+      c.beginPath()
+      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+      c.fillStyle = this.color
+      c.fill()
     }
 
-    if(this.y + this.radius > innerHeight || this.y - this.radius < 0){
-      this.dy = -this.dy
-    }
-
-    this.x += this.dx;
-    this.y += this.dy;
-
-    // interactivity   ->>> needs to prevent growing if outside window frame tho
-    if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50){
-      if(this.radius < maxRadius){
-        this.radius += 1
+    update = function(){
+      if(this.x + this.radius > innerWidth || this.x - this.radius < 0){
+        this.dx = -this.dx;
       }
-    } else if(this.radius > this.minRadius) {
-      this.radius -= 1
+
+      if(this.y + this.radius > innerHeight || this.y - this.radius < 0){
+        this.dy = -this.dy
+      }
+
+      this.x += this.dx;
+      this.y += this.dy;
+
+      // interactivity   ->>> needs to prevent growing if outside window frame tho
+      if (mouse.x - this.x - this.dx < 50 && mouse.x - this.x - this.dx > -50 && mouse.y - this.y - this.dy < 50 && mouse.y - this.y - this.dy > -50){
+        if(this.radius < maxRadius && (this.x + this.radius + this.dx < innerWidth) && (this.x - this.radius - this.dx > 0) && (this.y + this.radius + this.dy < innerHeight) && (this.y - this.radius -this.dy > 0)){
+          this.radius += 1
+        }
+      } else if(this.radius > this.minRadius) {
+        this.radius -= 1
+      }
+
+      this.draw()
     }
 
-    this.draw()
-  }
-}
+    drop = function(){
+      if(this.y + this.radius + this.dy >= innerHeight){
+        this.dy = -this.dy * 0.9
+      } else {
+        this.dy += 1;
+      }
+      this.y += this.dy;
+      this.draw()
+    }
 
-let circleArr = []
-
-function init(){
-  circleArr = []
-
-  for(let i = 0; i < 100; i++){
-    let radius = Math.random() * 3 + 1
-    let x = Math.random() * (innerWidth - radius * 2) + radius;
-    let y = Math.random() * (innerHeight - radius * 2) + radius;
-    let dx = (Math.random() - 0.5) * 4
-    let dy = (Math.random() - 0.5) * 4
-
-    circleArr.push(new Circle(x, y, dx, dy, radius))
   }
 
-}
+  let circleArr = []
 
-function animate(){
-  requestAnimationFrame(animate)
-  c.clearRect(0, 0, innerWidth, innerHeight)
+  function init(){
+    circleArr = []
 
-  for(let i = 0; i < circleArr.length; i++){
-    circleArr[i].update()
+    for(let i = 0; i < 400; i++){
+      let radius = Math.random() * 3 + 1
+      let x = Math.random() * (innerWidth - radius * 2) + radius;
+      let y = Math.random() * (innerHeight - radius * 2) + radius;
+      let dx = (Math.random() - 0.5) * 4
+      let dy = (Math.random() - 0.5) * 4
+
+      circleArr.push(new Circle(x, y, dx, dy, radius))
+    }
+
   }
 
-  c.fillStyle = "black";
-  c.font = "16px Arial";
-  c.textAlign = 'center'
-  c.textBaseline = 'middle'
-  c.fillText("header", innerWidth/2, innerHeight/2)
 
+  let animation;
+  let mode = true
 
-}
+  function animate(){
+    animation = requestAnimationFrame(animate)
+    c.clearRect(0, 0, innerWidth, innerHeight)
 
+    for(let i = 0; i < circleArr.length; i++){
+      circleArr[i].update()
+    }
+  }
 
-init()
+  function gravity(){
+    animation = requestAnimationFrame(gravity)
+    c.clearRect(0, 0, innerWidth, innerHeight)
 
-animate()
+    for(let i = 0; i < circleArr.length; i++){
+      circleArr[i].drop()
+    }
+  }
 
+  function reanimate(){
+    for(let i = 0; i < circleArr.length; i++){
+      circleArr[i].dx = (Math.random() - 0.5) * 4
+      circleArr[i].dy = (Math.random() - 0.5) * 4
 
+      if(circleArr[i].y >= innerHeight || circleArr[i].y + circleArr[i].radius >= innerHeight){
+        circleArr[i].y = innerHeight - 5
+        circleArr[i].dx = -3
+      }
+    }
+  }
 
+  init()
+  animate()
 
-////////////////
+  ////////////////
 
-let main = document.getElementById("main")
-let jokeDiv = document.getElementById("jokeDiv")
-
-let jokeBtn = document.getElementById("jokeBtn")
-
-jokeBtn.addEventListener('click', fetchJoke)
-
-
-function fetchJoke(){
-  fetch('https://icanhazdadjoke.com/', {
-    method: "GET",
-    headers: {
-      Accept: 'application/json',
-      "User-Agent": "https://github.com/mkim4247/personal-site/"
+  let charlie = document.getElementById('charlie')
+  charlie.addEventListener('click', function(){
+    cancelAnimationFrame(animation)
+    if(mode){
+      gravity()
+      mode = !mode
+    } else {
+      reanimate()
+      animate()
+      mode = !mode
     }
   })
-  .then(res => res.json())
-  .then(data => {
-    jokeDiv.innerText = data.joke
+
+  let landingHeader = document.getElementById('nameHeader')
+  landingHeader.addEventListener('click', function(){
+    let intro = document.getElementById('intro')
+    intro.scrollIntoView()
   })
+
+
+  ////////////////
+
+  let main = document.getElementById("main")
+  let jokeDiv = document.getElementById("jokeDiv")
+
+  let jokeBtn = document.getElementById("jokeBtn")
+
+  jokeBtn.addEventListener('click', fetchJoke)
+
+
+  function fetchJoke(){
+    fetch('https://icanhazdadjoke.com/', {
+      method: "GET",
+      headers: {
+        Accept: 'application/json',
+        "User-Agent": "https://github.com/mkim4247/personal-site/"
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      jokeDiv.innerText = data.joke
+    })
+  }
+
 }
+
 
 // function postJoke(joke) {
 //   let jokeDiv = document.createElement('div')
